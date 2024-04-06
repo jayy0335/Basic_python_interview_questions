f6cc3394c816b7cd0be3cfe169d474dcd0f7a086
Jazib Ali Khan <110601284+jayy0335@users.noreply.github.com>
2024-04-06 20:00:28 +0500

    Add files via upload

---
+++ sum_two_values_in_list.py
@@ -0,0 +1,23 @@
+def sum_number(l):
+    for i in l:
+        for j in l:
+            if i+j==0:
+                print("true")
+            else:
+                print("false")
+
+check=[1,2,3,-2]
+check=sum_number(check)
+
+
+
+
+
+
+
+
+
+
+
+
+
