2332bfd95759eebec649e1ed4539b912a646d38f
Jazib Ali Khan <110601284+jayy0335@users.noreply.github.com>
2024-04-08 21:00:33 +0500

    Add files via upload

---
+++ How_to_reverse_range.py
@@ -0,0 +1,15 @@
+# # Range in reverse case
+# # range()
+# # start:10
+# # condition:0
+# # increment:-1
+#
+for i in range(10,0,-1):
+    print(i)
+
+for i in range(10, -1, -2):
+    print(i)
+
+for i in range(10,0,-1):
+    print(i,"- 1 =",i-1)
+
